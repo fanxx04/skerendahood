d5954d4ad5fce7b79200d92c91ca89dcd6d2dad5
Nova Services <157920223+fanxx04@users.noreply.github.com>
2025-03-29 21:26:42 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ script_key = 'VYduAVBjFTcppIFDfCaRfOcQIcVsTqhu'
 getgenv().AutofarmSettings = {
     ["Fps"] = 10,
     ["InstaTP"] = true,
-    ["Underground"] = false,
+    ["Underground"] = true,
 
     ["Webhook"] = {
         ["URL"] = "https://discord.com/api/webhooks/1355521391218266222/8Luq8oDNFJ0Xf_lbk8QIZNMAIzgXbNR9l2AKM6nM5LndGuTQ9ELrtasB9C8t2gKEdGnC",
@@ -11,11 +11,11 @@ getgenv().AutofarmSettings = {
 
     ["Serverhop"] = {
         ["Cycle"]   = 1,        -- 1 = After dying once.
-        ["Time"]    = 60,        -- 1 = After 1 Minute.
+        ["Time"]    = 0,        -- 1 = After 1 Minute.
         ["Kick"]    = true,    -- true = After getting kicked.
         ["Blacklisted_IDs"] = { 1234567890, 1234567890 } -- If UserID was found ingame, Detects new joining players too.
     },
-    " LsDhc On Top!"
+    " warlocks atm farm - @snuffing "
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2f5a5d4b9fc7ed0f115580a53bfab777.lua"))()
 
