5edcf6b5327d059fd55eaf47aef591cb62123db1
Nova Services <157920223+fanxx04@users.noreply.github.com>
2025-03-31 16:22:42 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ getgenv().ControlSettings = {
     ["Host"] = "IsjeboyNL", -- Or UserID
     ["Fps"] = 5, 
     ["Prefix"] = "/", 
-    ["AdMessage"] = ".gg/Lsdhc"
+    ["AdMessage"] = "dahood money!!!",
+    " discord.gg/iku "
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ca81565eda5d99b30347fb11516b2f62.lua"))()
