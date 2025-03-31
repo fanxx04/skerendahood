0eda02cd78f7c68b64ca40549fd7ffa63c2c5189
Nova Services <157920223+fanxx04@users.noreply.github.com>
2025-03-31 16:12:12 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,22 +1,8 @@
 script_key = 'VYduAVBjFTcppIFDfCaRfOcQIcVsTqhu'
-getgenv().AutofarmSettings = {
-    ["Fps"] = 10,
-    ["InstaTP"] = true,
-    ["Underground"] = true,
-
-    ["Webhook"] = {
-        ["URL"] = "https://discord.com/api/webhooks/1355521391218266222/8Luq8oDNFJ0Xf_lbk8QIZNMAIzgXbNR9l2AKM6nM5LndGuTQ9ELrtasB9C8t2gKEdGnC",
-        ["Interval"] = 10
-    },
-
-    ["Serverhop"] = {
-        ["Cycle"]   = 5,        -- 1 = After dying once.
-        ["Time"]    = 0,        -- 1 = After 1 Minute.
-        ["Kick"]    = true,    -- true = After getting kicked.
-        ["Blacklisted_IDs"] = { 1234567890, 1234567890 } -- If UserID was found ingame, Detects new joining players too.
-    },
-    " warlocks atm farm - @snuffing "
+getgenv().ControlSettings = {
+    ["Host"] = "IsjeboyNL", -- Or UserID
+    ["Fps"] = 5, 
+    ["Prefix"] = "/", 
+    ["AdMessage"] = ".gg/Lsdhc"
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2f5a5d4b9fc7ed0f115580a53bfab777.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/fanxx04/sgimsa/refs/heads/main/boost.lua"))()
-
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ca81565eda5d99b30347fb11516b2f62.lua"))()
