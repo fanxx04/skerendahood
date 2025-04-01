0aa809e67375c19949e1486b4c0156a986b0f790
Nova Services <157920223+fanxx04@users.noreply.github.com>
2025-04-01 21:12:36 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,20 @@
 script_key = 'VYduAVBjFTcppIFDfCaRfOcQIcVsTqhu'
-getgenv().ControlSettings = {
-    ["Host"] = "IsjeboyNL", -- Or UserID
-    ["Fps"] = 5, 
-    ["Prefix"] = "/", 
-    ["AdMessage"] = "dahood money!!!",
-    " discord.gg/iku "
+getgenv().AutofarmSettings = {
+    ["Fps"] = 10,
+    ["InstaTP"] = true,
+    ["Underground"] = true,
+
+    ["Webhook"] = {
+        ["URL"] = "",
+        ["Interval"] = 10
+    },
+
+    ["Serverhop"] = {
+        ["Cycle"]   = 15,        -- 1 = After dying once.
+        ["Time"]    = 0,        -- 1 = After 1 Minute.
+        ["Kick"]    = true,    -- true = After getting kicked.
+        ["Blacklisted_IDs"] = { 1234567890, 1234567890 } -- If UserID was found ingame, Detects new joining players too.
+    },
+    " warlocks atm farm - @snuffing "
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ca81565eda5d99b30347fb11516b2f62.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2f5a5d4b9fc7ed0f115580a53bfab777.lua"))()
